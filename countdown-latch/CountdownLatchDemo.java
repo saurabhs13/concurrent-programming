9e29daebb18a45da766a7040e07a0aa1bdf54e65
Saurabh <saurabhs13@gmail.com>
2024-08-20 21:27:31 +0530

    Program to demonstrate countdown latch

--- countdown-latch/CountdownLatchDemo.java
+++ countdown-latch/CountdownLatchDemo.java
@@ -1,28 +1,29 @@
 
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.CountDownLatch;
 
 class ConferenceCall extends Thread{
 
-    private static Semaphore participantLicense = new Semaphore(4);
+    private static CountDownLatch latch = new CountDownLatch(11);
     
 
     public void run(){
         try {
-            participantLicense.acquire();
-            
+
+            System.out.println(this.getName()+" waiting for everyone to join to start the call");
+            latch.countDown();
+            latch.await();
             System.out.println(this.getName()+" joined the call");
-          
             Thread.sleep(2000);
         } catch (InterruptedException ie) {
             ie.printStackTrace();
         }finally{
             System.out.println(this.getName()+" leaving the call");
-            participantLicense.release();
+           
         }
         
     }
 }
-public class SemaphoreDemo{
+public class CountdownLatchDemo{
     public static void main(String[] args){
         for(int i=0;i<=10;i++){
             new ConferenceCall().start();
